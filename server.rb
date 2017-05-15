8dc0d617b83f1e329e2e04d6b7f8f6ec6f2e7342
Zian <zauvohi@gmail>
2017-05-15 17:20:49 -0500

    Added gitignore. Added helpers. Added common endpoints

--- server.rb
+++ server.rb
@@ -1,5 +1,37 @@
 require 'sinatra'
+require 'rom'
+require 'rom-sql'
+require_relative './helpers/helpers'
+require_relative './repos/ranking_repo'
+require_relative './serializers/ranking_serializer'
+# DB setup
+Dotenv.load
 
+container = ROM.container(:sql, ENV['DB_CONN'] + '/gw_rankings')
+@ranking_repo = RankingRepo.new(container)
+
+
+def half_if_too_many
+
+end
+
+# Endpoints
+before do
+  content_type 'application/json'
+end
 get '/' do
   'Hello there!'
+end
+
+get 'rankings/:edition/global/:ids' do
+
+  ids = params['ids'].map(&:to_i).keep_if { |id| id != 0 }
+end
+
+get 'rankings/:edition/global/:name' do
+
+end
+
+get 'rankings/:edition/:day/:id' do
+
 end
