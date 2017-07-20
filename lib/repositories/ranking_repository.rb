812c9ae087e74a833239d42c8aa8ca34df5ba318
Zian <zauvohi@gmail>
2017-07-20 13:15:19 -0500

    Removed unused code from ranking repository

--- lib/repositories/ranking_repository.rb
+++ lib/repositories/ranking_repository.rb
@@ -1,5 +1,4 @@
 require 'rom-repository'
-require 'pry'
 
 class RankingRepository < ROM::Repository[:rankings]
   relations :players, :editions
@@ -36,10 +35,6 @@ class RankingRepository < ROM::Repository[:rankings]
     ).to_a
   end
 
-  def full_by_id(id, edition)
-    rankings.where(player_id: id, edition: edition).to_a
-  end
-
   def by_name(name, edition)
     players = []
     ids = rankings.select(:player_id)
@@ -49,10 +44,5 @@ class RankingRepository < ROM::Repository[:rankings]
                   .pluck(:player_id)
     ids.each { |id| players.push(self.by_id(id, edition)) }
     players
-  end
-
-  def by_id_and_day(id, edition, day)
-    data = rankings.where(player_id: id, edition: edition, day: day).first
-    data.nil? ? nil : [data]
   end
 end
